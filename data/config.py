6f7a830a0c4aa8554ffe93d2c2ae39a3ea9a0618
Asadbek <asadbekoffical1202@gmail.com>
2025-02-28 16:50:32 +0500

    Update: Bots

--- data/config.py
+++ data/config.py
@@ -9,7 +9,7 @@ BOT_TOKEN = env.str("BOT_TOKEN")  # Bot Token
 ADMINS = env.list("ADMINS")  # adminlar ro'yxati
 API_KEY = env.str("API_KEY")
 ASSEMBLYAI_API_KEY = env.str("ASSEMBLYAI_API_KEY")
-
+DEEPGRAM_API_KEY = env.str("DEEPGRAM_API_KEY")
 
 DB_USER = env.str("DB_USER")
 DB_PASS = env.str("DB_PASS")
